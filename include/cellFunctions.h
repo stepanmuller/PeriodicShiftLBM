f4fb58278f53a081a8368fb8383b0f639a1f0c99
stepan <stepanmuller@outlook.com>
2026-04-25 13:43:06 +0200

    some local udev attempt

--- include/cellFunctions.h
+++ include/cellFunctions.h
@@ -233,3 +233,117 @@ __cuda_callable__ void getScalarTransport( float &scalarTransport, const float (
 	for ( int direction = 0; direction < 27; direction++ ) scalarTransport += f[direction] * T[direction];
 	scalarTransport = scalarTransport / fSum;
 }
+
+__host__ __device__ void getLocalDu( float (&f)[27], const float &nu, const float &SmagorinskyConstant, localDuStruct &localDu )
+{
+	float rho, ux, uy, uz;
+	getRhoUxUyUz( rho, ux, uy, uz, f );
+	//------------------------------------------------------------------------------------
+	//--------------------------- TRANSFORM TO CENTRAL MOMENTS ---------------------------
+	//------------------------------------------------------------------------------------
+
+	//Eq Geiger 2015(43)
+	//first part of the central moments transformation
+	const float k_aa0 = (f[21] + f[25]) + f[11];
+	const float k_ab0 = (f[8] + f[10]) + f[2];
+	const float k_ac0 = (f[24] + f[19]) + f[15];
+	const float k_ba0 = (f[14] + f[18]) + f[5];
+	const float k_bb0 = (f[4] + f[3]) + f[0];
+	const float k_bc0 = (f[17] + f[13]) + f[6];
+	const float k_ca0 = (f[20] + f[23]) + f[16];
+	const float k_cb0 = (f[9] + f[7]) + f[1];
+	const float k_cc0 = (f[26] + f[22]) + f[12];
+
+	const float k_aa1 = (f[21] - f[25]) - uz * k_aa0;
+	const float k_ab1 = (f[8] - f[10]) - uz * k_ab0;
+	const float k_ac1 = (f[24] - f[19]) - uz * k_ac0;
+	const float k_ba1 = (f[14] - f[18]) - uz * k_ba0;
+	const float k_bb1 = (f[4] - f[3]) - uz * k_bb0;
+	const float k_bc1 = (f[17] - f[13]) - uz * k_bc0;
+	const float k_ca1 = (f[20] - f[23]) - uz * k_ca0;
+	const float k_cb1 = (f[9] - f[7]) - uz * k_cb0;
+	const float k_cc1 = (f[26] - f[22]) - uz * k_cc0;
+
+	const float k_aa2 = (f[21] + f[25]) - 2.f * uz * (f[21] - f[25]) + uz * uz * k_aa0;
+	const float k_ab2 = (f[8] + f[10]) - 2.f * uz * (f[8] - f[10]) + uz * uz * k_ab0;
+	const float k_ac2 = (f[24] + f[19]) - 2.f * uz * (f[24] - f[19]) + uz * uz * k_ac0;
+	const float k_ba2 = (f[14] + f[18]) - 2.f * uz * (f[14] - f[18]) + uz * uz * k_ba0;
+	const float k_bb2 = (f[4] + f[3]) - 2.f * uz * (f[4] - f[3]) + uz * uz * k_bb0;
+	const float k_bc2 = (f[17] + f[13]) - 2.f * uz * (f[17] - f[13]) + uz * uz * k_bc0;
+	const float k_ca2 = (f[20] + f[23]) - 2.f * uz * (f[20] - f[23]) + uz * uz * k_ca0;
+	const float k_cb2 = (f[9] + f[7]) - 2.f * uz * (f[9] - f[7]) + uz * uz * k_cb0;
+	const float k_cc2 = (f[26] + f[22]) - 2.f * uz * (f[26] - f[22]) + uz * uz * k_cc0;
+
+	//Eq Geiger 2015(44)
+	//second part of the central moments transformation
+	const float k_a00 = (k_ac0 + k_aa0) + k_ab0;
+	const float k_b00 = (k_bc0 + k_ba0) + k_bb0;
+	const float k_c00 = (k_cc0 + k_ca0) + k_cb0;
+	const float k_a01 = (k_ac1 + k_aa1) + k_ab1;
+	const float k_b01 = (k_bc1 + k_ba1) + k_bb1;
+	const float k_c01 = (k_cc1 + k_ca1) + k_cb1;
+	const float k_a02 = (k_ac2 + k_aa2) + k_ab2;
+	const float k_b02 = (k_bc2 + k_ba2) + k_bb2;
+	const float k_c02 = (k_cc2 + k_ca2) + k_cb2;
+
+	const float k_a10 = (k_ac0 - k_aa0) - uy * k_a00;
+	const float k_b10 = (k_bc0 - k_ba0) - uy * k_b00;
+	const float k_c10 = (k_cc0 - k_ca0) - uy * k_c00;
+
+	const float k_a11 = (k_ac1 - k_aa1) - uy * k_a01;
+	const float k_b11 = (k_bc1 - k_ba1) - uy * k_b01;
+	const float k_c11 = (k_cc1 - k_ca1) - uy * k_c01;
+
+	const float k_a20 = (k_ac0 + k_aa0) - 2.f * uy * (k_ac0 - k_aa0) + uy * uy * k_a00;
+	const float k_b20 = (k_bc0 + k_ba0) - 2.f * uy * (k_bc0 - k_ba0) + uy * uy * k_b00;
+	const float k_c20 = (k_cc0 + k_ca0) - 2.f * uy * (k_cc0 - k_ca0) + uy * uy * k_c00;
+
+	//Eq Geiger 2015(45)
+	// third part of the central moments transformation
+	const float k_000 = (k_c00 + k_a00) + k_b00;
+	const float k_001 = (k_c01 + k_a01) + k_b01;
+	const float k_002 = (k_c02 + k_a02) + k_b02;
+	const float k_010 = (k_c10 + k_a10) + k_b10;
+	const float k_011 = (k_c11 + k_a11) + k_b11;
+	const float k_020 = (k_c20 + k_a20) + k_b20;
+
+	const float k_100 = (k_c00 - k_a00) - ux * k_000;
+	const float k_101 = (k_c01 - k_a01) - ux * k_001;
+	const float k_110 = (k_c10 - k_a10) - ux * k_010;
+
+	const float k_200 = (k_c00 + k_a00) - 2.f * ux * (k_c00 - k_a00) + ux * ux * k_000;
+
+	//------------------------------------------------------------------------------------
+	//------------------------------ CENTRAL MOM. TO CUMULANTS ---------------------------
+	//------------------------------------------------------------------------------------
+
+	//Eq Geiger 2015(47)
+	const float C_110 = k_110;
+	const float C_101 = k_101;
+	const float C_011 = k_011;
+
+	//Eq Geiger 2015(48)
+	const float C_200 = k_200;
+	const float C_020 = k_020;
+	const float C_002 = k_002;
+	
+	//------------------------------------------------------------------------------------
+	// -------------------------------CALCULATING LES OMEGA-------------------------------
+	//------------------------------------------------------------------------------------
+
+	float feq[27];
+	getFeq(rho, ux, uy, uz, feq);
+	float fneq[27];
+	getFneq(f, feq, fneq);
+	float omegaLES;
+	getOmegaLES(fneq, rho, nu, SmagorinskyConstant, omegaLES);
+
+	const float omega1 = omegaLES;
+	
+	localDu.duxdx = (0.5f * omega1) * (-2.f * C_200 + C_020 + C_002) + 0.5f * (rho - C_200 - C_020 - C_002);
+	localDu.duydy = localDu.duxdx + (1.5f * omega1) * (C_200 - C_020);
+	localDu.duzdz = localDu.duxdx + (1.5f * omega1) * (C_200 - C_002);
+	localDu.duXY = - (3.f * omega1) * C_110;
+	localDu.duYZ = - (3.f * omega1) * C_011;
+	localDu.duXZ = - (3.f * omega1) * C_101;
+}
